20da40fe56ccba61850bdd64e65421c2f2a1d971
rina4790 <rina4790karina@gmail.com>
2023-02-02 21:52:16 +0100

    feat: load swagger api from yaml file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as yaml from 'js-yaml';
+import { readFile } from 'fs/promises';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -7,7 +10,12 @@ const PORT = Number(process.env.PORT) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const document: any = yaml.load(
+    await readFile('./doc/api.yaml', { encoding: 'utf-8' }),
+  );
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(PORT);
+  console.log(PORT);
 }
 bootstrap();
